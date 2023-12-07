b61924d2a5819d7536b62571cc4ac05fc81d0b44
duongbuibd <duongbuibd@gmail.com>
2023-12-07 11:03:34 +0700

    build ui

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { HomePage } from "./pages";
+
+const App = () => {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+};
+export default App;
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Banner, Navbar } from "../components/index";
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+    </>
+  );
+};
+export default HomePage;
